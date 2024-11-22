61099dc56aa528c2e4a9754cf2f1a264948b4b93
gustavxferreira <gustavobusiness170@gmail.com>
2024-11-22 16:13:28 -0300

    Footer e header das views

---
+++ include/header.php
@@ -0,0 +1,29 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+<link rel="stylesheet" href="public/style/style.css">
+
+<title>Aluno+</title>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <a class="navbar-brand p-2" href="#">Aluno+</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Alunos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Matrículas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Cursos</a>
+      </li>
+     
+    </ul>
+  
+  </div>
+</nav>
