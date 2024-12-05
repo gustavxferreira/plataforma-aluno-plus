d391ebfbc4f4e36fdba5690214089acd333d8ca4
gustavxferreira <gustavobusiness170@gmail.com>
2024-12-05 16:44:33 -0300

    Removendo arquivos de configuração

--- include/footer.php
+++
@@ -1,2 +0,0 @@
-<script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
