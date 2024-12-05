e85086dab4b965c6df145b25bc7316c66bd86452
gustavxferreira <gustavobusiness170@gmail.com>
2024-12-05 17:01:05 -0300

    Deixando rotas mais concisas

--- routes/api.php
+++ routes/api.php
@@ -3,21 +3,27 @@
 use FastRoute\RouteCollector;
 
 return function (RouteCollector $r) {
+    
+    $r->addGroup('/api', function (RouteCollector $r) { 
+        
+        $r->addRoute('POST', '/logout', 'AuthController@logout');
+        $r->addRoute('POST', '/generate-token', 'AuthController@generateToken');
 
-    $r->addRoute('GET', '/api/students', 'StudentsController@index');
-    $r->addRoute('POST', '/api/students', 'StudentsController@store');
-    $r->addRoute('GET', '/api/students/{id:\d+}', 'StudentsController@show');
-    $r->addRoute('DELETE', '/api/students/{id:\d+}', 'StudentsController@destroy');
-    $r->addRoute('PUT', '/api/students/{id:\d+}', 'StudentsController@update');
-
-    $r->addRoute('GET', '/api/courses', 'CoursesController@index');
-    $r->addRoute('POST', '/api/courses', 'CoursesController@store');
-    $r->addRoute('GET', '/api/courses/{id:\d+}', 'CoursesController@show');
-    $r->addRoute('PUT', '/api/courses/{id:\d+}', 'CoursesController@update');
-
-    $r->addRoute('GET', '/api/enrollments', 'EnrollmentsController@index');
-    $r->addRoute('GET', '/api/enrollments/{id:\d+}', 'EnrollmentsController@show');
-    $r->addRoute('POST', '/api/enrollments', 'EnrollmentsController@store');
-    $r->addRoute('PATCH', '/api/enrollments/{id:\d+}', 'EnrollmentsController@update');
-    $r->addRoute('DELETE', '/api/enrollments/{id:\d+}', 'EnrollmentsController@destroy');
+        $r->addRoute('GET', '/students', 'StudentsController@index');
+        $r->addRoute('POST', '/students', 'StudentsController@store');
+        $r->addRoute('GET', '/students/{id:\d+}', 'StudentsController@show');
+        $r->addRoute('DELETE', '/students/{id:\d+}', 'StudentsController@destroy');
+        $r->addRoute('PUT', '/students/{id:\d+}', 'StudentsController@update');
+    
+        $r->addRoute('GET', '/courses', 'CoursesController@index');
+        $r->addRoute('POST', '/courses', 'CoursesController@store');
+        $r->addRoute('GET', '/courses/{id:\d+}', 'CoursesController@show');
+        $r->addRoute('PUT', '/courses/{id:\d+}', 'CoursesController@update');
+    
+        $r->addRoute('GET', '/enrollments', 'EnrollmentsController@index');
+        $r->addRoute('GET', '/enrollments/{id:\d+}', 'EnrollmentsController@show');
+        $r->addRoute('POST', '/enrollments', 'EnrollmentsController@store');
+        $r->addRoute('PATCH', '/enrollments/{id:\d+}', 'EnrollmentsController@update');
+        $r->addRoute('DELETE', '/enrollments/{id:\d+}', 'EnrollmentsController@destroy');
+    });
 };
--- routes/web.php
+++ routes/web.php
@@ -2,85 +2,31 @@
 
 use FastRoute\RouteCollector;
 
-function checkSessionWeb($currentRoute)
-{
-    session_start();
-    if ($currentRoute === '/login') {
-        return;
-    }
-    if (!isset($_SESSION['token'])) {
-        http_response_code(401);
-        echo "<script>window.location.href = '/login'</script>";
-        exit();
-    }
-}
-
 return function (RouteCollector $r) {
 
-    $r->addRoute('GET', '/login', function () {
-        include 'views/login/login.php';
-    });
-
-    $r->addRoute('POST', '/generate-token', function () {
-        include 'src/auth/generateToken.php';
-    });
+    $r->addRoute('GET', '/login', fn() => view('login.login', false));
 
     $r->addRoute('GET', '/logout', function () {
-        include 'src/auth/destroySession.php';
+        destroySession();
+        header('Location: /login');
+        exit();
     });
 
-    $r->addRoute('GET', '/not-found', function () {
-        include 'views/404.php';
-    });
+    $r->addRoute('GET', '/not-found', fn() => view('errors.404', false));
 
     // index
-    $r->addRoute('GET', '/', function () {
-        checkSessionWeb('/');
-        include 'views/students/students.php';
-    });
+    $r->addRoute('GET', '/', fn() => view('students.students'));
 
-    $r->addRoute('GET', '/alunos', function () {
-        checkSessionWeb('/alunos');
-        include 'views/students/students.php';
-    });
-    
-    $r->addRoute('GET', '/alunos/criar', function () {
-        checkSessionWeb('/alunos/criar');
-        include 'views/students/forms/createStudents.php';
-    });
+    $r->addRoute('GET', '/alunos', fn() => view('students.students'));
+    $r->addRoute('GET', '/alunos/criar', fn() => view('students.forms.createStudents'));
+    $r->addRoute('GET', '/alunos/editar/{id:\d+}', fn() => view('students.edit.editStudents'));
 
-    $r->addRoute('GET', '/alunos/editar/{id:\d+}', function () {
-        checkSessionWeb('/alunos/editar/{id:\d+}');
-        include 'views/students/edit/editStudents.php';
-    });
-    
-    $r->addRoute('GET', '/cursos', function () {
-        checkSessionWeb('/cursos');
-        include 'views/courses/courses.php';
-    });
+    $r->addRoute('GET', '/cursos', fn() => view('courses.courses'));
+    $r->addRoute('GET', '/cursos/criar', fn() => view('courses.forms.createCourses'));
+    $r->addRoute('GET', '/cursos/editar/{id:\d+}', fn() => view('courses.edit.editCourse'));
 
-    $r->addRoute('GET', '/cursos/criar', function () {
-        checkSessionWeb('/cursos/criar');
-        include 'views/courses/forms/createCourses.php';
-    });
+    $r->addRoute('GET', '/matriculas', fn() => view('enrollments.enrollments'));
+    $r->addRoute('GET', '/matriculas/criar', fn() => view('enrollments.forms.createEnrollments'));
+    $r->addRoute('GET', '/matriculas/editar/{id:\d+}', fn() => view('enrollments.edit.editEnrollments'));
 
-    $r->addRoute('GET', '/cursos/editar/{id:\d+}', function () {
-        checkSessionWeb('/cursos/editar/{id:\d+}');
-        include 'views/courses/edit/editCourse.php';
-    });
-
-    $r->addRoute('GET', '/matriculas', function () {
-        checkSessionWeb('/matriculas');
-        include 'views/enrollments/enrollments.php';
-    });
-
-    $r->addRoute('GET', '/matriculas/criar', function () {
-        checkSessionWeb('/matriculas/criar');
-        include 'views/enrollments/forms/createEnrollments.php';
-    });
-
-    $r->addRoute('GET', '/matriculas/editar/{id:\d+}', function () {
-        checkSessionWeb('/matriculas/editar/{id:\d+}');
-        include 'views/enrollments/edit/editEnrollments.php';
-    });
 };  
